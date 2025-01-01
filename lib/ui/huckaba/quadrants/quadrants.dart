f8bd58b6e38748c0588414c2de93e2249be3d149
Abdulqadir8604 <web.aq8604@gmail.com>
2025-01-02 01:36:03 +0530

    huckaba page fix 1

--- lib/ui/huckaba/quadrants/quadrants.dart
+++ lib/ui/huckaba/quadrants/quadrants.dart
@@ -77,180 +77,227 @@ class _HuckabaQuadsState extends State<HuckabaQuads> {
       ),
       body: OrientationBuilder(
         builder: (context, orientation) {
-          return orientation == Orientation.landscape
-              ? SingleChildScrollView(
-                child: Row(
+          return SingleChildScrollView(
+            child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(child: _buildInputSection()),
-                      if (report.isNotEmpty)
-                        Expanded(
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 500),
-                            curve: Curves.easeInOut,
-                            child: _buildReportSection(),
+                      const SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              widget.hucabaData["title"] as String,
+                              style: TextStyle(
+                                fontSize: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge
+                                    ?.fontSize,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(16),
+                        color: Theme.of(context).colorScheme.primary,
+                        child: Text(
+                          "Measure the following on the study model:",
+                          style: TextStyle(
+                            fontSize:
+                                Theme.of(context).textTheme.bodyLarge?.fontSize,
+                            color: Theme.of(context).colorScheme.onPrimary,
                           ),
                         ),
-                    ],
-                  ),
-              )
-              : SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      _buildInputSection(),
-                      if (report.isNotEmpty)
-                        AnimatedContainer(
-                          duration: const Duration(milliseconds: 500),
-                          curve: Curves.easeInOut,
-                          child: _buildReportSection(),
+                      ),
+                      const SizedBox(height: 16),
+                      MTextField(
+                        controller: x1Controller,
+                        label:
+                            'Mesiodistal width of ${widget.hucabaData["x1"].toString()}',
+                        hint: 'mm',
+                        onChanged: (_) {},
+                      ),
+                      const SizedBox(height: 16),
+                      Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(16),
+                        color: Theme.of(context).colorScheme.primary,
+                        child: Text(
+                          "Measure the following on the IOPA:",
+                          style: TextStyle(
+                            fontSize:
+                                Theme.of(context).textTheme.bodyLarge?.fontSize,
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
                         ),
+                      ),
+                      const SizedBox(height: 16),
+                      MTextField(
+                        controller: x2Controller,
+                        label:
+                            'Mesiodistal width of ${widget.hucabaData["x2"].toString()}',
+                        hint: 'mm',
+                        onChanged: (_) {},
+                      ),
+                      MTextField(
+                        controller: y2Controller,
+                        label:
+                            'Mesiodistal width of ${widget.hucabaData["y2"].toString()}',
+                        hint: 'mm',
+                        onChanged: (_) {},
+                      ),
+                      const SizedBox(height: 16),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: MButton(
+                            color: Theme.of(context).colorScheme.tertiary,
+                            onPressed: calculateY1,
+                            text: 'Calculate',
+                            width: 200,
+                            height: 50,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      if (errorMessage != null)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            errorMessage!,
+                            style: const TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      if (report.isNotEmpty) ...[
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(16),
+                          color: Theme.of(context).colorScheme.primary,
+                          child: Text(
+                            "Report:",
+                            style: TextStyle(
+                              fontSize: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.fontSize,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "Space available:",
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .bodyLarge
+                                        ?.fontSize,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  report["spaceAvailable"] ?? "",
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium
+                                        ?.fontSize,
+                                    fontWeight: FontWeight.bold,
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Divider(),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "Apparent Width of Unerupted Premolar:",
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .bodyLarge
+                                        ?.fontSize,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  report["apparentWidthOfUneruptedPremolar"] ??
+                                      "",
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium
+                                        ?.fontSize,
+                                    fontWeight: FontWeight.bold,
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Divider(),
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(16),
+                          color: Theme.of(context).colorScheme.primary,
+                          child: Text(
+                            "Prediction:",
+                            style: TextStyle(
+                              fontSize: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.fontSize,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            report["prediction"] ?? "",
+                            style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontSize: Theme.of(context)
+                                  .textTheme
+                                  .headlineMedium
+                                  ?.fontSize,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                        ),
+                      ],
                     ],
                   ),
-                );
+          );
         },
-      ),
-    );
-  }
-
-  Widget _buildInputSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 8),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                widget.hucabaData["title"] as String,
-                style: TextStyle(
-                  fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(height: 8),
-        _buildSectionTitle("Measure the following on the study model:"),
-        const SizedBox(height: 16),
-        MTextField(
-          controller: x1Controller,
-          label: 'Mesiodistal width of ${widget.hucabaData["x1"].toString()}',
-          hint: 'mm',
-          onChanged: (_) {},
-        ),
-        const SizedBox(height: 16),
-        _buildSectionTitle("Measure the following on the IOPA:"),
-        const SizedBox(height: 16),
-        MTextField(
-          controller: x2Controller,
-          label: 'Mesiodistal width of ${widget.hucabaData["x2"].toString()}',
-          hint: 'mm',
-          onChanged: (_) {},
-        ),
-        MTextField(
-          controller: y2Controller,
-          label: 'Mesiodistal width of ${widget.hucabaData["y2"].toString()}',
-          hint: 'mm',
-          onChanged: (_) {},
-        ),
-        const SizedBox(height: 16),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: SizedBox(
-            width: double.infinity,
-            child: MButton(
-              color: Theme.of(context).colorScheme.tertiary,
-              onPressed: calculateY1,
-              text: 'Calculate',
-              width: 200,
-              height: 50,
-            ),
-          ),
-        ),
-        const SizedBox(height: 16),
-        if (errorMessage != null) ...[
-          const SizedBox(height: 16),
-          Text(
-            errorMessage!,
-            style:
-                const TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ],
-    );
-  }
-
-  Widget _buildReportSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildSectionTitle("Report:"),
-        const SizedBox(height: 16),
-        _buildReportRow("Space available:", report["spaceAvailable"] ?? ""),
-        const Divider(),
-        _buildReportRow("Apparent Width of Unerupted Premolar:",
-            report["apparentWidthOfUneruptedPremolar"] ?? ""),
-        const Divider(),
-        _buildSectionTitle("Prediction:"),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            report["prediction"] ?? "",
-            style: TextStyle(
-              fontStyle: FontStyle.italic,
-              fontSize: Theme.of(context).textTheme.headlineMedium?.fontSize,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSectionTitle(String title) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      color: Theme.of(context).colorScheme.primary,
-      child: Text(
-        title,
-        style: TextStyle(
-          fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
-          color: Theme.of(context).colorScheme.onPrimary,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildReportRow(String label, String value) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Text(
-              label,
-              style: TextStyle(
-                fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Text(
-              value,
-              style: TextStyle(
-                fontSize: Theme.of(context).textTheme.headlineMedium?.fontSize,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
